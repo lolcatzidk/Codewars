7dc9f954ede94dc8e16e34286e77973c3ece993f
David Cardenas <davidcardenas423@gmail.com>
2025-02-16 16:40:38 -0500

    Update Replace_With_Alphabet_Position.js
    Welcome.
    
    In this kata you are required to, given a string, replace every letter with its position in the alphabet.
    
    If anything in the text isn't a letter, ignore it and don't return it.
    
    "a" = 1, "b" = 2, etc.
    
    Example
    Input = "The sunset sets at twelve o' clock."
    Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"

--- Replace_With_Alphabet_Position.js
+++ Replace_With_Alphabet_Position.js
@@ -1,12 +1,16 @@
 function alphabetPosition(text) {
   justLetters = text.replace(/[^a-z]/gi,""); //removes everything that is not a letter
+  // [^a-z] -> Matches anything that is NOT a letter.
+  // g -> Global flag (affects the whole string).
+  // i -> Case-insensitive (treats uppercase and lowercase the same).. for later use
+  
   let result = "";
   
   for (let i = 0; i<justLetters.length; i++) {
     result += getLetterNum(justLetters.charAt(i)); //Concatenates the resulting number (with a space) to newText
   }
   
-  return result.trim();
+  return result.trim(); //the space at the beginning is trimmed with this
 }
 function getLetterNum(letter) {
   const alphabetArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
